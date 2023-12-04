38b05473c761e2e2fd94be91590f5a542310e758
sajonaro <alex.kunich@gmail.com>
2023-12-04 15:57:25 +0200

    added test consumer

--- utils/consume-topic-cli.sh
+++ utils/consume-topic-cli.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env sh
 
-docker compose exec -i pulsar-server  bin/pulsar-client consume wso-data-stream -s test-sub1 -n 0 
+docker compose exec -i pulsar-server  bin/pulsar-client consume \
+  -n 0  \
+  -p Earliest \
+  -s "test-sub1" \
+  wso-data-stream 
 
-#docker compose exec -i pulsar-server  bin/pulsar-ctl subscriptions list persistent://public/default/wso-data-stream
